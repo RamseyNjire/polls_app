04f023d40eeb365c34958d1098e95dacd72f429b
Ramsey Njire <ramseynjire@gmail.com>
2021-04-19 17:24:28 +0300

    finish writing #results method for questions model and ensure they work

--- app/models/question.rb
+++ app/models/question.rb
@@ -19,4 +19,28 @@ class Question < ApplicationRecord
         through: :answer_choices,
         source: :responses
     )
+
+    def results
+        # results_hash = {}
+        # Below is the code that would trigger an N+1 query
+
+        # self.answer_choices.each do |answer_choice|
+        #     results_hash[answer_choice.text] = answer_choice.responses.count
+        # end
+
+        # Below is the code that would solve the N+1 problem by prefetching responses with answer choices
+
+        # answer_choices = self.answer_choices.includes(:responses)
+        # answer_choices.each do |answer_choice|
+        #     results_hash[answer_choice.text] = answer_choice.responses.length
+        # end
+        answer_choices = self.answer_choices
+        .select("answerchoices.text, COUNT(responses.id) AS response_count")
+        .left_outer_joins(:responses).group("answerchoices.id")
+
+        answer_choices.inject({}) do |results_hash, answer_choice|
+        results_hash[answer_choice.text] = answer_choice.response_count
+        results_hash
+        end
+    end
 end
